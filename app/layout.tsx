1a877e494924ae392758440bac4569710284eee4
Ball15124 <98155756+Ball15124@users.noreply.github.com>
2024-10-25 21:04:48 +0700

    Fixed padding body layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${notoSansThai.className} min-h-screen bg-[#efeeeb]`}>
+      <body className={`${inter.className} ${notoSansThai.className} pt-20 min-h-screen bg-[#efeeeb]`}>
       <Navbar />
         {children}
       </body>
