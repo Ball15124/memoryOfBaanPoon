df5bb7e904e4d2d622c4c55206f9f382b08d9e76
Ball15124 <98155756+Ball15124@users.noreply.github.com>
2024-11-28 07:49:38 +0700

    IOS Never die

--- app/page.tsx
+++ app/page.tsx
@@ -19,18 +19,18 @@ export default function Home() {
   const handleCulture = (id: number) => {
     router.push(`/cultural-heritage/${id}`);
   };
-  // const [isIOS, setIsIOS] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
 
-  // useEffect(() => {
-  //   const userAgent = window.navigator.userAgent.toLowerCase();
-  //   if (
-  //     userAgent.includes("iphone") ||
-  //     userAgent.includes("ipod") ||
-  //     userAgent.includes("ipad")
-  //   ) {
-  //     setIsIOS(true);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    if (
+      userAgent.includes("iphone") ||
+      userAgent.includes("ipod") ||
+      userAgent.includes("ipad")
+    ) {
+      setIsIOS(true);
+    }
+  }, []);
 
   useEffect(() => {
     // Access localStorage only on the client
@@ -96,8 +96,7 @@ export default function Home() {
       <main className="flex w-full flex-col pb-5 relative top-[-80px]">
         {/* Replace 'path/to/your/image.jpg' with the actual path to your image */}
         <div
-          className={`flex flex-col w-full bg-[url('/assests/images/หน้าแรก(1).JPG')] bg-no-repeat items-center sticky top-0 h-[1100px] bg-scroll bg-center bg-cover`}
-        >
+          className={`flex flex-col sticky top-0 w-full h-[1100px] bg-[url('/assests/images/หน้าแรก(1).JPG')] bg-scroll bg-center bg-no-repeat bg-cover items-center ${isIOS ? 'ios-background-fix' : ''}`}>
           <h1
             className={`${poppins.className} text-[28px] sm:text-[30px] lg:text-[48px] text-[#C53232] font-light drop-shadow-lg text-center mt-[300px] animate-fade-in`}
           >
