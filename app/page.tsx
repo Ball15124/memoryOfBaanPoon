d75df2a77d18efa89929cdba1ed407f654fbebf8
Ball15124 <98155756+Ball15124@users.noreply.github.com>
2024-11-28 07:51:54 +0700

    The Struggle of IOS

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
       <main className="flex w-full flex-col pb-5 relative top-[-80px]">
         {/* Replace 'path/to/your/image.jpg' with the actual path to your image */}
         <div
-          className={`flex flex-col sticky top-0 w-full h-[1100px] bg-[url('/assests/images/หน้าแรก(1).JPG')] bg-scroll bg-center bg-no-repeat bg-cover items-center ${isIOS ? 'ios-background-fix' : ''}`}>
+          className={`flex flex-col w-full bg-[url('/assests/images/หน้าแรก(1).JPG')] bg-no-repeat items-center ${isIOS ? 'ios-background-fix' : 'sticky top-0 h-[1100px] bg-scroll bg-center bg-cover'}`}>
           <h1
             className={`${poppins.className} text-[28px] sm:text-[30px] lg:text-[48px] text-[#C53232] font-light drop-shadow-lg text-center mt-[300px] animate-fade-in`}
           >
