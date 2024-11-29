e9be971e5df04a52b7c99dc5932310a026206df8
Ball15124 <98155756+Ball15124@users.noreply.github.com>
2024-11-29 20:27:17 +0700

    Adjust footer

--- app/components/navigation/footer.tsx
+++ app/components/navigation/footer.tsx
@@ -15,9 +15,29 @@ const Footer = () => {
   const pathname = usePathname(); // Get the current pathname
   const router = useRouter();
 
+  const handleNavigation = (sectionId: string) => {
+    // Navigate to the home page
+    if (pathname === "/") {
+      const targetElement = document.getElementById(sectionId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    } else {
+      router.push("/");
+
+      // Delay the scroll action to ensure the home page loads
+      setTimeout(() => {
+        const targetElement = document.getElementById(sectionId);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 100); // Adjust delay if necessary
+    }
+  };
+
   return (
     <div
-      className={`w-full h-80 lg:h-52 relative bottom-0 z-50 px-5 lg:px-32 py-5 lg:py-10 transition-all duration-[1000ms] ${
+      className={`w-full h-full relative bottom-0 z-50 px-5 lg:px-32 py-5 lg:py-10 transition-all duration-[1000ms] ${
         pathname === "/dsad" ? "opacity-0 h-0" : "opacity-100 h-20"
       } ${pathname !== "/dsad" ? "bg-[#c53232]" : "bg-transparent"}`}
     >
@@ -33,11 +53,11 @@ const Footer = () => {
           >
             MEMORY OF<p className="leading-none">BAAN POON</p>
           </h1>
-          <div className="hidden lg:block h-full w-[2px] bg-black ml-8" />
+          <div className="hidden lg:block h-52 w-[1px] bg-white ml-8" />
         </div>
-        <div className="flex w-[70%] justify-center lg:justify-end text-center lg:text-start">
+        <div className="flex w-[70%] justify-center lg:justify-end text-center lg:text-start lg:place-content-end">
           {/* Desktop Links */}
-          <ul className="flex flex-col lg:flex-row h-full">
+          <ul className="flex flex-col lg:flex-row text-[20px] xl:text-[25px] place-content-end h-full">
             <li className="h-full">
               <button
                 className={` text-white hover:text-gray-500 p-1 lg:p-4 transition-colors duration-300 tracking-widest hover:bg-white h-full`}
@@ -68,17 +88,37 @@ const Footer = () => {
                 เรื่องเล่าตำนาน
               </button>
             </li>
+            <li>
+              <button
+                className={`${
+                  pathname.startsWith("/cultural-heritage") ? "text-gray-400" : "text-white"
+                } hover:text-gray-500 p-1 lg:p-4 transition-colors duration-300 hover:bg-white tracking-widest h-full`}
+                onClick={() => handleNavigation("cultural-heritage")}
+              >
+                มรดกวัฒนธรรม
+              </button>
+            </li>
+            <li>
+              <button
+                className={`${
+                  pathname.startsWith("/gallery") ? "text-gray-400" : "text-white"
+                } hover:text-gray-500 p-1 lg:p-4 transition-colors duration-300 hover:bg-white tracking-widest h-full`}
+                onClick={() => handleNavigation("gallery")}
+              >
+                Picture of Baan Poon
+              </button>
+            </li>
           </ul>
         </div>
-        <div className="flex flex-row h-full items-center">
-          <div className="hidden lg:block h-full w-[2px] bg-black mr-10" />
-          <div className="flex flex-col">
-            <button className={`p-2 ${outfit.className} text-[12px] text-white`}>
-              CONTACT US
+        <div className="flex flex-row h-full">
+          <div className="hidden lg:block h-52 w-[1px] bg-white mr-10" />
+          <div className="flex flex-col justify-center">
+            <button className={`text-white text-center xl:text-start text-[20px] xl:text-[25px]`}>
+              ติดต่อเรา
             </button>
-            <div className={`p-2 ${outfit.className} text-[12px] bg-white flex flex-row items-center`}>
+            <div className={`py-2 px-3 ${outfit.className} text-[15px] bg-white flex flex-row items-center space-x-2`}>
              <ContactSupportIcon fontSize="medium"/>
-              097-020-0803
+              <p>097-020-0803</p>
             </div>
           </div>
         </div>
